64af6ee7e30b80459d4fbfb67267f7c55ba5955a
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-10-15 11:33:34 +0900

    test

--- testWebkit1/ViewController.swift
+++ testWebkit1/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webkit: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://testapp-9ee4c.web.app/")
+        let request = URLRequest(url: url!)
+        webkit.load(request)
+        
+        
+        
     }
 
 
